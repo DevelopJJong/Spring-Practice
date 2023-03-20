b4219cc5a63ffc94ac54540f3b96f1d9470ae3ec
DevelopJJong <jjong___e@naver.com>
2023-03-20 21:56:08 +0900

    valid 추가

--- sbb/sbb/src/main/java/com/mysite/sbb/controller/QuestionController.java
+++ sbb/sbb/src/main/java/com/mysite/sbb/controller/QuestionController.java
@@ -3,15 +3,16 @@ package com.mysite.sbb.controller;
 import java.util.List;
 
 import com.mysite.sbb.domain.Question;
+import com.mysite.sbb.form.QuestionForm;
 import com.mysite.sbb.repository.QuestionRepository;
 import com.mysite.sbb.service.QuestionService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -32,5 +33,19 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";// 질문 저장후 질문목록으로 이동
     }
 }
