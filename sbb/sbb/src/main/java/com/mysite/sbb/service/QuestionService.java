631019d14a56cb8b9c8dbc0e380b1d9fe1bf4457
DevelopJJong <jjong___e@naver.com>
2023-03-22 15:49:43 +0900

    날짜순으로 정리하는 서비스 추가

--- sbb/sbb/src/main/java/com/mysite/sbb/service/QuestionService.java
+++ sbb/sbb/src/main/java/com/mysite/sbb/service/QuestionService.java
@@ -1,12 +1,17 @@
 package com.mysite.sbb.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.domain.Question;
 import com.mysite.sbb.repository.QuestionRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -35,6 +40,12 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
     public void delete(Integer id){
        questionRepository.deleteById(id);
